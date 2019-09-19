39c78828fbd744cf7dd808b349f59aaa66f11480
阿男 <l.weinan@gmail.com>
2019-09-19 16:57:14 +0800

    .

--- src/test/java/irmc/TagsApiIT.java
+++ src/test/java/irmc/TagsApiIT.java
@@ -1,43 +1,30 @@
-package dev.isikemre.test.irmc;
+package irmc;
 
-import irmc.FooApi;
-import irmc.ProblematicApi;
-import irmc.Tag;
-import irmc.TagsApi;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 // https://issues.jboss.org/browse/RESTEASY-2335
 public class TagsApiIT {
 
-    public static final String HTTP_LOCALHOST_8080 = "http://localhost:8080";
+   public static final String HTTP_LOCALHOST_8080 = "http://localhost:8080";
 
-    @Test
+   @Test
 //    @Disabled
-    void testGet() {
-        TagsApi tagsApi = RestClientBuilder.newBuilder().baseUri(URI.create(HTTP_LOCALHOST_8080)).build(TagsApi.class);
+   void testGet() {
+      TagsApi tagsApi = RestClientBuilder.newBuilder().baseUri(URI.create(HTTP_LOCALHOST_8080)).build(TagsApi.class);
 //        Tag tag = tagsApi.get("some-thing", "some-id");
 
 //        assertNull(tag);
-    }
+   }
 
-    @Test
-    void testFoo() {
+   @Test
+   void testFoo() {
 
-        // reproducer:
-        FooApi api = RestClientBuilder.newBuilder().baseUri(URI.create(HTTP_LOCALHOST_8080)).build(FooApi.class);
-    }
-
-    @Test
-    void testBad() {
-        ProblematicApi api = RestClientBuilder.newBuilder().baseUri(URI.create(HTTP_LOCALHOST_8080)).build(ProblematicApi.class);
-    }
-
+      // reproducer:
+      FooApi api = RestClientBuilder.newBuilder().baseUri(URI.create(HTTP_LOCALHOST_8080)).build(FooApi.class);
+   }
 
 
 }
