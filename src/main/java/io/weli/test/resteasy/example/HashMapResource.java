0c146313acdb015658c902f0d2cda322c683d05a
Weinan Li <l.weinan@gmail.com>
2020-03-30 06:57:08 +0800

    update

--- src/main/java/io/weli/test/resteasy/example/HashMapResource.java
+++ src/main/java/io/weli/test/resteasy/example/HashMapResource.java
@@ -8,29 +8,31 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.HashMap;
 import java.util.stream.Collectors;
 
 @Path("/map")
-public class HashMapResource implements HashMapResourceInterface {
+public class HashMapResource {
     @POST
-    public String map(HashMap<String, String> params) {
-        return params.keySet().stream()
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void map(HashMap<String, String> params) {
+        System.out.println(params.keySet().stream()
                 .map(key -> key + "=" + params.get(key))
-                .collect(Collectors.joining(", ", "{", "}"));
+                .collect(Collectors.joining(", ", "{", "}")));
     }
 
     public static void main(String[] args) {
         Client client = ClientBuilder.newClient();
+
         WebTarget target = client.target("http://localhost:8080/app/map");
-        ResteasyWebTarget rtarget = (ResteasyWebTarget) target;
-
-        HashMapResourceInterface mapResource = rtarget.proxy(HashMapResourceInterface.class);
-        mapResource.map(new HashMap<String, String>() {{
+        Invocation.Builder builder = target.request(MediaType.APPLICATION_JSON);
+        builder.post(Entity.entity(new HashMap() {{
             put("answer", "42");
-        }});
-
+        }}, MediaType.APPLICATION_JSON));
     }
 }
